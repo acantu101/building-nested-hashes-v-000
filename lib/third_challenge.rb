41540d8be199c4720698d39e40736b7680a601cb
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-28 01:40:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,7 +12,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => { :name => "Lord Capulet", :age "50"
         #your key/value pairs here
@@ -27,5 +27,5 @@ def third_challenge
    }
   }
 
-  
+
 end
