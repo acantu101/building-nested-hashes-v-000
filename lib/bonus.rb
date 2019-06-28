06dafc5db9503d3698f84e0f6b30a42395023c4e
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-28 02:31:46 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,6 @@ def bonus
 
   #code your solution here:
 [:montague][:hero][:status]= ["dead"]
-capulet = {"status" => "alive"}
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
