fe52693fa97782c9de17119a1a29b107e4077eb0
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-28 02:52:42 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,11 +21,7 @@ def bonus
   }
 
   #code your solution here:
-epic_tragedy.collect do |main, att|
-  if att = "status: "alive""
-    then att = "status: "dead""
-  end
-end
+epic_tragedy[:montague][:hero_friends][:status] = "dead"
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
