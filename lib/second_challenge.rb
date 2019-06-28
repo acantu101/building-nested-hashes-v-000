34cb38d0a1dd26303704f13c62a1d33dcd4422aa
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-28 01:32:51 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,21 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      }, 
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => {},
+      :heroine_friends => []
+      },
     :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => {},
+      :heroine_friends => []
+      }
       #your key/value pairs here!
-      }}
+      }
 
-  
+
 end
