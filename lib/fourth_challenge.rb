2dbac18d3c58905696fc35f6e24741a93acba25f
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-28 02:08:46 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -11,9 +11,11 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends =>
+      :heroine_friends => [{:names => "Steven", :age => "30", :attitude => "confused"},
+        {:names => "Nurse", :age => "44", :attitude => "worried"}]
+   },
         #your hashes here!
-      ]
+      
    }
 }
 
