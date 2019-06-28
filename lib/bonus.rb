bebc943e47a1ea100e36f4f8d4dd558d5bd55682
alejandra cantu <alejandra.cantu13@gmail.com>
2019-06-28 02:36:35 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
   #code your solution here:
-epic_tragedy[:heroine][:status] <=> [:hero][:status]
+epic_tragedy[:heroine][:status] <=> epic_tragedy[:hero][:status]
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
